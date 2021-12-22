85852a830d3dc14d2164f5a4115d19743c411393
VeerMT <71698491+VRmt231@users.noreply.github.com>
2021-12-22 19:04:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ song = "";
 song2 = "";
 leftWristX = 0;
 rightWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
+score_LeftWrist = 0;
+score_RightWrist = 0;
 function setup() {
       canvas = createCanvas(600,500);
       canvas.center();
@@ -32,16 +36,20 @@ function play(){
     song2.play();
 }
 
-
-function gotPoses(results){
-    if(results.length > 0 ){
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
-        leftWristX = results[0].pose.leftWrist.x
-        leftWristY = results[0].pose.leftWrist.y
-        rightWristX = results[0].pose.rightWrist.x
-        rightWristY = results[0].pose.rightWrist.y
-        console.log("leftWristX - " + leftWristX + "leftWristY - " + leftWristY );
-        console.log("rightWristX - " + rightWristX + "rightWristY - " + rightWristY );
+        score_LeftWrist = results[0].pose.keypoints[9].score;
+        score_RightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + score_LeftWrist);
+        console.log("scoreRightWrist = " + score_RightWrist);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("leftWristX - " + leftWristX + "leftWristY - " + leftWristY);
+        console.log("rightWristX - " + rightWristX + "rightWristY - " + rightWristY);
+
         
     }
 }
